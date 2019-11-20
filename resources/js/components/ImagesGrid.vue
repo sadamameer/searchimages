6bbfa13cb9dd87d6ecafa409666e2d87ba06975d
Ameer Hamza <ameerhamza7110@gmail.com>
2019-11-20 23:32:13 +0500

    No images found

--- resources/js/components/ImagesGrid.vue
+++ resources/js/components/ImagesGrid.vue
@@ -1,46 +1,53 @@
 <template>
     <div class="container-fluid">
-        <div class="row mt-3 mb-4" v-if="images.length && !FirstLoaded">
-            <div class="col">
-                <span>Showing results from <b>{{ (page * per_page) - (per_page - 1) }}-{{ page * per_page }}</b></span>
+        <div v-if="images.length" class="mt-3">
+            <div class="row mt-3 mb-4" v-if="images.length && !FirstLoaded">
+                <div class="col">
+                    <span>Showing results from <b>{{ (page * per_page) - (per_page - 1) }}-{{ page * per_page }}</b></span>
+                </div>
+                <div class="col text-right">
+                    <span><b>{{ total_results }}</b> matching images found</span>
+                    <i class="fa fa-object-ungroup ml-3 cursor-pointer" @click="SwitchView()"></i>
+                </div>
             </div>
-            <div class="col text-right">
-                <span><b>{{ total_results }}</b> matching images found</span>
-                <i class="fa fa-object-ungroup ml-3 cursor-pointer" @click="SwitchView()"></i>
-            </div>
-        </div>
-        <div class="row" v-if="images.length">
-            <div class="col-md-4 p-0" v-for="image in images" :key="image.id" :id="image.id">
-                <div class="content">
-                    <span>
-                    <div class="content-overlay"></div>
-                    <img class="content-image" :src="image.url_m">
-                    <div class="content-details fadeIn-bottom">
-                        <h3 class="content-title">{{image.ownername.toUpperCase()}}</h3>
-                        <div class="d-inline-flex">
-                            <span><i class="fa fa-heart"></i><p>{{image.count_faves}}</p></span>
-                            <span><i class="fa fa-comments"></i><p>{{image.count_comments}}</p></span>
-                            <span><i class="fa fa-eye"></i><p>{{image.views}}</p></span>
+            <div class="row" v-if="images.length">
+                <div class="col-md-4 p-0" v-for="image in images" :key="image.id" :id="image.id">
+                    <div class="content">
+                        <span>
+                        <div class="content-overlay"></div>
+                        <img class="content-image" :src="image.url_m">
+                        <div class="content-details fadeIn-bottom">
+                            <h3 class="content-title">{{image.ownername.toUpperCase()}}</h3>
+                            <div class="d-inline-flex">
+                                <span><i class="fa fa-heart"></i><p>{{image.count_faves}}</p></span>
+                                <span><i class="fa fa-comments"></i><p>{{image.count_comments}}</p></span>
+                                <span><i class="fa fa-eye"></i><p>{{image.views}}</p></span>
+                            </div>
+                            <div class="mt-2">
+                                <a :href="image.url_o" target="_blank"><button class="btn btn-outline-light btn-sm">View Original</button></a>
+                            </div>
+                            <div class="mt-3">
+                                <p class="content-text mb-0">{{ image.datetaken }}</p>
+                                <span class="badge badge-success" v-if="image.originalformat">{{ image.originalformat.toUpperCase() }}</span>
+                            </div>
                         </div>
-                        <div class="mt-2">
-                            <a :href="image.url_o" target="_blank"><button class="btn btn-outline-light btn-sm">View Original</button></a>
-                        </div>
-                        <div class="mt-3">
-                            <p class="content-text mb-0">{{ image.datetaken }}</p>
-                            <span class="badge badge-success" v-if="image.originalformat">{{ image.originalformat.toUpperCase() }}</span>
-                        </div>
+                        </span>
                     </div>
-                    </span>
+                </div>
+            </div>
+            <div class="row mt-4 mb-4" v-if="images.length && !FirstLoaded">
+                <div class="col" v-if="PrevPage">
+                    <button class="btn btn-outline-success" @click="ChangePageNumber(page-1)">Previous</button>
+                </div>
+                <div class="col text-right" v-if="NextPage">
+                    <button class="btn btn-outline-success" @click="ChangePageNumber(page+1)">Next</button>
                 </div>
             </div>
         </div>
-        <div class="row mt-4 mb-4" v-if="images.length && !FirstLoaded">
-            <div class="col" v-if="PrevPage">
-                <button class="btn btn-outline-success" @click="ChangePageNumber(page-1)">Previous</button>
-            </div>
-            <div class="col text-right" v-if="NextPage">
-                <button class="btn btn-outline-success" @click="ChangePageNumber(page+1)">Next</button>
-            </div>
+        <div class="container text-center mt-5 p-5" v-else>
+            <img src="404.png" alt="Nothing Found" class="m-auto">
+            <br><br>
+            <h5>No images found. Please try another query.</h5>
         </div>
     </div>
 </template>
